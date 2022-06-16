8f46e07ff54e118d1d78e02e5f3e728dc5da1b94
Akobir <akobirbahronov07@gmail.com>
2022-06-16 22:33:42 +0500

    section_1

---
+++ src/coffees/coffees.service.ts
@@ -0,0 +1,43 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Coffee } from './entities/coffee.entity';
+
+@Injectable()
+export class CoffeesService {
+  private coffees: Coffee[] = [
+    {
+      id: 1,
+      name: 'Someone',
+      brand: 'SomeBrand',
+      flavors: ['chocolate', 'vanilla'],
+    },
+  ];
+  findAll() {
+    return this.coffees;
+  }
+
+  findOne(id: string) {
+    // throw 'A random error';
+    const coffee = this.coffees.find((item) => item.id === +id);
+    if (!coffee) {
+      throw new NotFoundException(`Coffee #${id} not found`);
+    }
+    return coffee;
+  }
+
+  create(createCoffeeDto: any) {
+    this.coffees.push(createCoffeeDto);
+    return createCoffeeDto;
+  }
+  update(id: string, updatedDto: any) {
+    const existingCoffee = this.findOne(id);
+    if (existingCoffee) {
+      // update the existing entity
+    }
+  }
+  remove(id: string) {
+    const coffeeIndex = this.coffees.findIndex((item) => item.id === +id);
+    if (coffeeIndex >= 0) {
+      this.coffees.splice(coffeeIndex, 1);
+    }
+  }
+}
